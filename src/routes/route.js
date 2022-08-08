7f4ecc4c2a09ffb4bd74311252846b00f796883e
alesha <aleshasayyad499@gmail.com>
2022-08-08 14:27:38 +0530

    8 aug

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const underScore = require('underscore')
+const loDash = require('lodash')
 const abc = require('../introduction/intro')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
+
+  
+
+    let weekend = ['sat', 'sun']
+    let result = underScore.first(weekend)
+    console.log('Underscore example result is', result)
+
+    let year = ['jan', 'feb', 'march','april', 'may', 'jun', 'july', 'aug', 'sep', 'oct', 'nov', 'dec']
+    console.log(loDash.chunk(year,3))
+
+    let oddNum = [ 1,3,5,7,9,11,13,15,17,19]
+    console.log(loDash.tail(oddNum))
+
+    let arrays = [ 1,2,2,3,4]
+    console.log(loDash.union(arrays))
+
+    let object =  [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    console.log(loDash.fromPairs(object))
+    
     abc.printName()
     res.send('My second ever api!')
 });
