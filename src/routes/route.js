3a0fa3dc8b8adaca0a99b68ce85cbeb33a59fa49
alesha <aleshasayyad499@gmail.com>
2022-08-14 10:47:33 +0530

    14aug

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,58 +1,64 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ],
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ],
-       },
-   ]
+// let players =
+//    [
+//        {
+//            "name": "manish",
+//            "dob": "1/1/1995",
+//            "gender": "male",
+//            "city": "jalandhar",
+//            "sports": [
+//                "swimming"
+//            ]
+//        },
+//        {
+//            "name": "gopal",
+//            "dob": "1/09/1995",
+//            "gender": "male",
+//            "city": "delhi",
+//            "sports": [
+//                "soccer"
+//            ],
+//        },
+//        {
+//            "name": "lokesh",
+//            "dob": "1/1/1990",
+//            "gender": "male",
+//            "city": "mumbai",
+//            "sports": [
+//                "soccer"
+//            ],
+//        },
+//    ]
 
-   router.post('/players', function (req, res) {
+//    router.post('/players', function (req, res) {
 
-       let newPlayer = req.body
-       let newPlayersName=newPlayer.name
-       let isNameRepeated=false
-       for(let i=0; i<players.length;i++){
-        if(players[i].name==newPlayersName){
-            isNameRepeated=true;
+//        let newPlayer = req.body
+//        let newPlayersName=newPlayer.name
+//        let isNameRepeated=false
+//        for(let i=0; i<players.length;i++){
+//         if(players[i].name==newPlayersName){
+//             isNameRepeated=true;
           
-            break;
-        }
-       }
-       if(isNameRepeated){
-        res.send("This player was already exist")
-       }else{
-        players.push(newPlayer)
-        res.send(players)
-       }
-    });
+//             break;
+//         }
+//        }
+//        if(isNameRepeated){
+//         res.send("This player was already exist")
+//        }else{
+//         players.push(newPlayer)
+//         res.send(players)
+//        }
+//     });
       
 
 
-module.exports = router;
+// module.exports = router;
 // adding this comment for no reason
+const express=require('express');
+const bodyParsor=require('bodyParser');
+const route=require('./routes/route.js');
+const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlrncoded)
